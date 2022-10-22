038a7f5dcdf132da3a88fcbda3d2cdb2aa27d2d3
Yabs Get <yabstegad@gmail.com>
2022-10-21 20:21:18 -0600

    Ran go fmt

--- boredbook.go
+++ boredbook.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
-  "strings"
-  "runtime"
-  "os/exec"
-  "bufio"
-  "os"
-  "time"
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
 
-  "github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 )
 
 // https://gist.github.com/hyg/9c4afcd91fe24316cbf0
@@ -19,14 +19,14 @@ func openbrowser(url string) {
 	var err error
 
 	switch runtime.GOOS {
-  	case "linux":
-  		err = exec.Command("xdg-open", url).Start()
-  	case "windows":
-  		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-  	case "darwin":
-  		err = exec.Command("open", url).Start()
-  	default:
-  		err = fmt.Errorf("unsupported platform")
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
 	}
 	if err != nil {
 		log.Fatal(err)
@@ -35,174 +35,174 @@ func openbrowser(url string) {
 
 // https://pkg.go.dev/github.com/PuerkitoBio/goquery
 func exploreSite(url string) {
-  // Request the HTML page.
-  res, err := http.Get(url)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer res.Body.Close()
-  if res.StatusCode != 200 {
-    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-  }
+	// Request the HTML page.
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
 
-  // Load the HTML document
-  doc, err := goquery.NewDocumentFromReader(res.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  openedSite := make(map[string]bool)
+	openedSite := make(map[string]bool)
 
-  openedSitesCount := 0
-  // Find the review items
-  doc.Find("a").EachWithBreak(func(i int, s *goquery.Selection) bool {
+	openedSitesCount := 0
+	// Find the review items
+	doc.Find("a").EachWithBreak(func(i int, s *goquery.Selection) bool {
 		// For each item found, get the title
-    if openedSitesCount % 5 == 0 {
-      fmt.Printf(
-        "You have explored %d sites, do you want to open the next 5? (yes/no)\n",
-        openedSitesCount)
+		if openedSitesCount%5 == 0 {
+			fmt.Printf(
+				"You have explored %d sites, do you want to open the next 5? (yes/no)\n",
+				openedSitesCount)
 
-      var yesNo string
-      _, err := fmt.Scanln(&yesNo)
-      if err != nil {
-        log.Fatal(err)
-      }
+			var yesNo string
+			_, err := fmt.Scanln(&yesNo)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-      if yesNo == "yes" {
-        // continue opening sites to explore
-      } else {
-        // exit exploring site
-        return false
-      }
-    }
+			if yesNo == "yes" {
+				// continue opening sites to explore
+			} else {
+				// exit exploring site
+				return false
+			}
+		}
 
-    OUTTER:
-    for j := 0; j < len(s.Nodes); j++ {
-        for k := 0; k < len(s.Nodes[j].Attr); k++ {
-          //fmt.Printf("SAttr %d: %s\n", k, s.Nodes[j].Attr[k])
-          href := s.Nodes[j].Attr[k].Val
-          if (strings.HasPrefix(href, "http") && !openedSite[href]) {
-              openbrowser(href)
-              openedSite[href] = true
-              openedSitesCount++
-              continue OUTTER
-          }
-        }
-    }
-    return true
+	OUTTER:
+		for j := 0; j < len(s.Nodes); j++ {
+			for k := 0; k < len(s.Nodes[j].Attr); k++ {
+				//fmt.Printf("SAttr %d: %s\n", k, s.Nodes[j].Attr[k])
+				href := s.Nodes[j].Attr[k].Val
+				if strings.HasPrefix(href, "http") && !openedSite[href] {
+					openbrowser(href)
+					openedSite[href] = true
+					openedSitesCount++
+					continue OUTTER
+				}
+			}
+		}
+		return true
 	})
 }
 
 func getSitesToSkip() map[string]bool {
-  skipSites := make(map[string]bool)
+	skipSites := make(map[string]bool)
 
-  skipExploreFile, err := os.Open("skipExplore.txt")
-  defer skipExploreFile.Close()
+	skipExploreFile, err := os.Open("skipExplore.txt")
+	defer skipExploreFile.Close()
 
-  if err != nil {
-    log.Printf("Could not find or open skipExplore.txt: %s\n", err)
-  } else {
-    scanner := bufio.NewScanner(skipExploreFile)
-    for scanner.Scan() {
-      siteToSkip := scanner.Text()
-      skipSites[siteToSkip] = true
-    }
+	if err != nil {
+		log.Printf("Could not find or open skipExplore.txt: %s\n", err)
+	} else {
+		scanner := bufio.NewScanner(skipExploreFile)
+		for scanner.Scan() {
+			siteToSkip := scanner.Text()
+			skipSites[siteToSkip] = true
+		}
 
-    if err := scanner.Err(); err != nil {
-      log.Fatal(err)
-    }
-  }
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
 
-  return skipSites
+	return skipSites
 }
 
 func extractURLsFromHTML() {
-  err := exec.Command("./extractURLs.sh", "bookmarks.html").Start()
-  if err != nil {
-    log.Fatal(err)
-  }
+	err := exec.Command("./extractURLs.sh", "bookmarks.html").Start()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  // sleep to let the script run before resuming program
-  time.Sleep(2 * time.Second)
+	// sleep to let the script run before resuming program
+	time.Sleep(2 * time.Second)
 }
 
 func getSitesToExplore(skipSites map[string]bool) []string {
-  // https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go
-  urlsFile, err := os.Open("urls.txt")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer urlsFile.Close()
+	// https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go
+	urlsFile, err := os.Open("urls.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer urlsFile.Close()
 
-  sites := make([]string, 0, 0)
+	sites := make([]string, 0, 0)
 
-  scanner := bufio.NewScanner(urlsFile)
-  for scanner.Scan() {
-    url := scanner.Text()
-    if !skipSites[url] {
-      sites = append(sites, url)
-    }
-  }
+	scanner := bufio.NewScanner(urlsFile)
+	for scanner.Scan() {
+		url := scanner.Text()
+		if !skipSites[url] {
+			sites = append(sites, url)
+		}
+	}
 
-  if err := scanner.Err(); err != nil {
-    log.Fatal(err)
-  }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-  return sites
+	return sites
 }
 
 func exploreSites(sites []string) {
-  // https://pkg.go.dev/os#example-OpenFile-Append
-  skipNextTimeFile, err := os.OpenFile("skipExplore.txt",
-    os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// https://pkg.go.dev/os#example-OpenFile-Append
+	skipNextTimeFile, err := os.OpenFile("skipExplore.txt",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  for i := 0; i < len(sites); i++ {
-    siteToExplore := sites[i]
+	for i := 0; i < len(sites); i++ {
+		siteToExplore := sites[i]
 
-    // https://gosamples.dev/read-user-input/
-    fmt.Printf("Do you want to explore %s ? (yes/no/skip/exit)\n", siteToExplore)
+		// https://gosamples.dev/read-user-input/
+		fmt.Printf("Do you want to explore %s ? (yes/no/skip/exit)\n", siteToExplore)
 
-    var yesNoSkipExit string
-    _, err := fmt.Scanln(&yesNoSkipExit)
-    if err != nil {
-      log.Fatal(err)
-    }
+		var yesNoSkipExit string
+		_, err := fmt.Scanln(&yesNoSkipExit)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-    if yesNoSkipExit == "yes" {
-      fmt.Printf("Visiting site: %s\n", siteToExplore)
-      openbrowser(siteToExplore)
-      exploreSite(siteToExplore)
-    } else if yesNoSkipExit == "no" {
-      fmt.Printf("Not exploring site.\n")
-    } else if yesNoSkipExit == "skip" {
-      if _, err := skipNextTimeFile.Write([]byte(siteToExplore + "\n")); err != nil {
-        skipNextTimeFile.Close()
-        log.Fatal(err)
-      }
-    } else {
-      fmt.Printf("Exiting program. Goodbye.\n")
-      break;
-    }
-  }
-  if err := skipNextTimeFile.Close(); err != nil {
-    log.Fatal(err)
-  }
+		if yesNoSkipExit == "yes" {
+			fmt.Printf("Visiting site: %s\n", siteToExplore)
+			openbrowser(siteToExplore)
+			exploreSite(siteToExplore)
+		} else if yesNoSkipExit == "no" {
+			fmt.Printf("Not exploring site.\n")
+		} else if yesNoSkipExit == "skip" {
+			if _, err := skipNextTimeFile.Write([]byte(siteToExplore + "\n")); err != nil {
+				skipNextTimeFile.Close()
+				log.Fatal(err)
+			}
+		} else {
+			fmt.Printf("Exiting program. Goodbye.\n")
+			break
+		}
+	}
+	if err := skipNextTimeFile.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
 
-  fmt.Printf("Extracting urls from bookmarks.html\n")
-  extractURLsFromHTML()
+	fmt.Printf("Extracting urls from bookmarks.html\n")
+	extractURLsFromHTML()
 
-  fmt.Printf("Gathering sites to skip during exploring.\n")
-  skipSites := getSitesToSkip()
+	fmt.Printf("Gathering sites to skip during exploring.\n")
+	skipSites := getSitesToSkip()
 
-  fmt.Printf("Retrieving sites to explore.\n")
-  sites := getSitesToExplore(skipSites)
+	fmt.Printf("Retrieving sites to explore.\n")
+	sites := getSitesToExplore(skipSites)
 
-  fmt.Printf("Starting exploration of sites.\n")
-  exploreSites(sites)
+	fmt.Printf("Starting exploration of sites.\n")
+	exploreSites(sites)
 }
